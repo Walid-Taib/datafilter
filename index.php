0295f44d67b13f3ff264b5befbfca5885863653e
walid taib <walidtaib44@gmail.com>
2022-11-28 19:43:26 -0800

    connected to db

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
 
+
+
+
+
+
+
+
+
+
 $mot=$_POST['field-keywords'];
 $auteur=$_POST['field-author'];
 $titre=$_POST['field-title'];
@@ -9,12 +18,29 @@ $collection=$_POST['field-collection'];
 $rebrique=$_POST['node'];
 $format=$_POST['field-binding_browse-bin'];
 $dates=$_POST['field-dateop'];
+$mois=$_POST['field-datemod'];
+$annees=$_POST['field-dateyear'];
+$sort=$_POST['sort'];
+
+if($mot=="livre"){
+    print "hello world";
+}
 
 
 
 
 
+$servername="localhost";
+$username="root";
+$password="";
 
+$connection=new mysqli($servername,$username,$password);
+if($connection->connect_error){
+    echo("error");
+}
+else{
+    echo('Connected');
+}
 
 
 
